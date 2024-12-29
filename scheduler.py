d017af183e056a7e961669343865e23ad969d1a7
Dylan Roberts <dylangardnerroberts@gmail.com>
2024-12-29 15:26:50 -0500

    Wrap Schedulers into a Scheduler class that accepts algorithm arg

--- scheduler.py
+++ scheduler.py
@@ -44,54 +44,6 @@ class Task():
             raise Exception("Completion Time is None")
         return self.turnaround_time() - self.burst_time
 
-def fcfs_scheduler(tasks):
-    time = 0
-    while any(not task.completed for task in tasks):
-        task_executed = False
-        for task in tasks:
-            if not task.completed and time >= task.arrival_time: # Only run uncompleted tasks that have arrived
-                print(f"Time:{time} - Executing {task.name}")
-                task.execute()
-                task_executed = True
-                if task.completed:
-                    print(f"Time:{time+1} - Task {task.name} Complete")
-                    task.completion_time = time + 1
-                # Only run 1 task per CPU Tick
-                break
-    
-        if not task_executed:
-            # Check if a task is arriving in the next tick
-            if any(task.arrival_time > time for task in tasks):
-                print(f"Time:{time} - CPU Idle")
-        
-        # Time ticks forward
-        time += 1
-
-def stcf_scheduler(tasks: List[Task]):
-    time = 0
-    while any(not task.completed for task in tasks):
-        task_executed = False
-        
-        for task in sorted(tasks, key=lambda task: task.remaining_time):
-            if not task.completed and time >= task.arrival_time: # Only run uncompleted tasks that have arrived
-                print(f"Time:{time} - Executing {task.name}")
-                task.execute()
-                task_executed = True
-                if task.completed:
-                    print(f"Time:{time+1} - Task {task.name} Complete")
-                    task.completion_time = time + 1
-                # Only run 1 task per CPU Tick
-                break
-    
-        if not task_executed:
-            # Check if a task is arriving in the next tick
-            if any(task.arrival_time > time for task in tasks):
-                print(f"Time:{time} - CPU Idle")
-        
-        # Time ticks forward
-        time += 1
-
-
 def print_stats(tasks):         
     for task in tasks:
         print(f"Task {task.name} turnaround time is {task.turnaround_time()}")
@@ -99,7 +51,39 @@ def print_stats(tasks):
     
     print(f"Average turnaround time is {round(sum([task.turnaround_time() for task in tasks])/len(tasks), 3)}")
 
+class Scheduler:
+    def __init__(self, scheduling_algorithm):
+        self.scheduling_algorithm = scheduling_algorithm
 
+    def run(self, tasks):
+        time = 0
+        while any(not task.completed for task in tasks):
+            task_executed = False
+            
+            for task in self.scheduling_algorithm(tasks):
+                if not task.completed and time >= task.arrival_time: # Only run uncompleted tasks that have arrived
+                    print(f"Time:{time} - Executing {task.name}")
+                    task.execute()
+                    task_executed = True
+                    if task.completed:
+                        print(f"Time:{time+1} - Task {task.name} Complete")
+                        task.completion_time = time + 1
+                    # Only run 1 task per CPU Tick
+                    break
+        
+            if not task_executed:
+                # Check if a task is arriving in the next tick
+                if any(task.arrival_time > time for task in tasks):
+                    print(f"Time:{time} - CPU Idle")
+            
+            # Time ticks forward
+            time += 1
+
+def stcf_scheduler(tasks):
+    return sorted(tasks, key=lambda task: task.remaining_time)
+
+def fcfs_scheduler(tasks):
+    return tasks
 
 def main():
     tasks = [
@@ -107,7 +91,9 @@ def main():
         Task("t2", t2, arrival_time=10, burst_time=10),
         Task("t3", t3, arrival_time=10, burst_time=10),
     ]
-    stcf_scheduler(tasks)
+    scheduler = Scheduler(stcf_scheduler)
+
+    scheduler.run(tasks)
 
     print_stats(tasks)
 
