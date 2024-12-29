e943454b5e77e47af53c324828dc56796b630f48
Dylan Roberts <dylangardnerroberts@gmail.com>
2024-12-29 14:53:16 -0500

    Added some stats

--- scheduler.py
+++ scheduler.py
@@ -1,52 +1,85 @@
 
 
-from time import sleep
-
 def t1():
-    sleep(1)
+    pass
 
 def t2():
-    sleep(1)
+    pass
 
 def t3():
-    sleep(1)
+    pass
 
 class Task():
-    def __init__(self, name, func, atime=0, rtime=1, priority=0):
-        self.name       = name
-        self.func       = func
-        self.atime      = atime
-        self.rtime      = rtime
-        self.priority   = priority
-        self.completed  = False
-
+    def __init__(self, name, func, arrival_time=0, burst_time=1, priority=0):
+        self.name           = name
+        self.func           = func
+        self.arrival_time   = arrival_time
+        self.remaining_time = burst_time
+        self.priority       = priority
+        self.completed      = False
+        self.burst_time     = burst_time
 
     def execute(self):
-        if self.rtime > 0:
-            print(f"Executing {self.name}")
+        if self.remaining_time > 0:
             self.func()
-            self.rtime -= 1
-        else:
-            print(f"Task {self.name} completed")
+            self.remaining_time -= 1
+        if self.remaining_time == 0:
             self.completed = True
 
-def schedule(tasks):
-    while(len(tasks)):
-        for task in tasks:
-            task.execute()
-        
-        # Remove completed tasks
-        tasks = [task for task in tasks if task.completed is False]
+    def turnaround_time(self):
+        if not self.completed:
+            raise Exception("Task not Completed")
+        if not self.completion_time:
+            raise Exception("Completion Time is None")
+        return self.completion_time - self.arrival_time
 
-                
+    def waiting_time(self):
+        if not self.completed:
+            raise Exception("Task not Completed")
+        if self.completion_time is None:
+            raise Exception("Completion Time is None")
+        return self.turnaround_time() - self.burst_time
+
+def fcfs_scheduler(tasks):
+    time = 0
+    while any(not task.completed for task in tasks):
+        task_executed = False
+        for task in tasks:
+            if not task.completed and time >= task.arrival_time: # Only run uncompleted tasks that have arrived
+                print(f"Time:{time} - Executing {task.name}")
+                task.execute()
+                task_executed = True
+                if task.completed:
+                    print(f"Time:{time+1} - Task {task.name} Complete")
+                    task.completion_time = time + 1
+                # Only run 1 task per CPU Tick
+                break
+    
+        if not task_executed:
+            # Check if a task is arriving in the next tick
+            if any(task.arrival_time > time for task in tasks):
+                print(f"Time:{time} - CPU Idle")
+        
+        # Time ticks forward
+        time += 1
+
+def print_stats(tasks):         
+    for task in tasks:
+        print(f"Task {task.name} turnaround time is {task.turnaround_time()}")
+        print(f"Task {task.name} wait time is {task.waiting_time()}")
+    
+    print(f"Average turnaround time is {sum([task.turnaround_time() for task in tasks])/len(tasks)}")
+
 
 def main():
     tasks = [
-        Task("t1", t1),
-        Task("t2", t2),
-        Task("t3", t3),
+        Task("t1", t1, arrival_time=0, burst_time=10),
+        Task("t2", t2, arrival_time=0, burst_time=10),
+        Task("t3", t3, arrival_time=0, burst_time=10),
     ]
-    schedule(tasks)
+    fcfs_scheduler(tasks)
+
+    print_stats(tasks)
 
 if __name__ == "__main__":
     main()
